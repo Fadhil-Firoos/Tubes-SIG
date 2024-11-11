d5e43338f08b083951bd6c342ee169d64261f426
adislksn <adi.120140038@student.itera.ac.id>
2024-11-11 18:46:58 +0700

    feat: coordinate migration, models, controller

---
+++ app/Http/Controllers/Map/SavePointController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Map;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SavePointController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(coordinate $coordinate)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(coordinate $coordinate)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, coordinate $coordinate)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(coordinate $coordinate)
+    {
+        //
+    }
+}
