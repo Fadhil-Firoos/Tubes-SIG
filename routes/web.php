ef7dd716ecd8da6fc26fa6e4ab69cdc9571c26e9
Fadhil-Firoos <fadhilfiroos@gmail.com>
2024-11-11 20:22:38 +0700

    update for view batas sukarame linestring

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/map', function () {
+    return view('map');
+});
 
 Route::get('/dashboard', function () {
     return view('admin.dashboard');
