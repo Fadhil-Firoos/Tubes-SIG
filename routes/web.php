8bed55b8aaea0c60350939223cb8a8a2385a298e
Henry Carnegie <99797029+Henrycarnegie@users.noreply.github.com>
2024-11-29 21:00:02 +0700

    Revert "Development"

--- routes/web.php
+++ routes/web.php
@@ -6,21 +6,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/map', function () {
-    return view('map');
-});
 
 Route::get('/dashboard', function () {
-    return view('admin.dashboard');
+    return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::get('/users-create', function () {
-    return view('admin.users-create');
-})->middleware(['auth', 'verified'])->name('users-create');
-
-Route::get('/users-index', function () {
-    return view('admin.users-index');
-})->middleware(['auth', 'verified'])->name('users-index');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -29,4 +18,3 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
-require __DIR__.'/mapping.php';
