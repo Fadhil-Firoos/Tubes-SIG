d00615c0b234a80675b08b04d09d8e0113e5a3d3
nikola4512 <nikolaarinanda01@gmail.com>
2024-12-23 11:41:55 +0700

    ubah tipis-tipis

--- resources/views/mapping/index.blade.php
+++ resources/views/mapping/index.blade.php
@@ -68,7 +68,6 @@
                         value="{{ request('start_date') ? \Carbon\Carbon::parse(request('start_date'))->format('Y-m-d\TH:i') : '' }}"
                         onchange="updateQuery()"
                     >
-                    <!-- <input id="start_date" datepicker datepicker-buttons datepicker-autoselect-today type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Select date"> -->
                 </div>
 
             </form>
